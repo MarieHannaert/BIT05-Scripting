4ea456e402f0e6fbe11fbcdb2cfeefad44fc5dbf
Hannaert Marie <marie.hannaert@student.howest.be>
2023-11-21 17:25:14 +0100

    updated folder

--- 2. Data-types-and-operations/exercise2.1.py
+++ 2. Data-types-and-operations/exercise2.1.py
@@ -10,12 +10,12 @@ print(text.lower())
 #############################################################################################
 #Write a Python script that takes a sequence and a motif, searches the motif and shows the position (not index) of the motif in the sequence
 ##########################################################################################
-sequence=input("what is the sequence?\n")
-motif=input("what is the motif?\n")
+sequence=input("what is the sequence?\n ")
+motif=input("what is the motif?\n ")
 print(sequence.find(motif)+1)
 ############################################################################################
 #Write a Python script that takes a sequence and a cleavage site (split)and displays the sequence fragments after cleavage (split)
 ################################################################################
 sequence2=input("what is the second sequence?\n")
 cleavage=input("what is the cleavage site?\n")
-print(sequence2[int(cleavage):])
+print(sequence2.split())
