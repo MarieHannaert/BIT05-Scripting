249b9494da113e5b8006f55abc016535ff2d3883
Hannaert Marie <marie.hannaert@student.howest.be>
2023-11-24 10:53:48 +0100

    add files to github folder

---
+++ 2. Data-types-and-operations/exercise2.3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+charged = ["Arg", "Lys"]
+print(charged)
+secondcharged = ["Asp", "Glu"]
+charged.extend(secondcharged)
+print(charged)
+
---
+++ 2. Data-types-and-operations/exercise2.4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+mySet = {1, 2, [3, 4]}
+#a set is inmutabale, this also means you can't add mutuble elemens 
+print(mySet)
+
+
+Sprot = set()
+Sprot.add('NP_001304113')
+print(Sprot)
+Sprot.add('NP_001304114')
+print("RefSeq id in the protein set:\n".format(Sprot))
