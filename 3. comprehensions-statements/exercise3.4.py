abea12ebecb0a55069a87804a210b05ed80f30a4
Hannaert Marie <marie.hannaert@student.howest.be>
2023-11-28 12:24:08 +0100

    Changes to be committed: 	modified:   3. comprehensions-statements/exercise3.4.py 	modified:   3. comprehensions-statements/exercise3.5.py 	new file:   3. comprehensions-statements/exercises-3.4.py 	new file:   3. comprehensions-statements/exercises-3.5.py

--- 3. comprehensions-statements/exercise3.4.py
+++ 3. comprehensions-statements/exercise3.4.py
@@ -5,4 +5,17 @@ print("List of capitals:\n=================")
 for k, v in countries.items():
     print("{}".format(v))
 
-astriks=int(input("give a number for astriksjes: "))
+n=5
+for i in range(n):
+    for j in range(i):
+        print('*', end="")
+    print('')
+for i in range(n,0,-1):
+    for j in range(i):
+        print('*', end="")
+    print('')
+    
+
+
+
+    
--- 3. comprehensions-statements/exercise3.5.py
+++ 3. comprehensions-statements/exercise3.5.py
@@ -1,5 +1,38 @@
 #!/usr/bin/python3
 import math
-thelist = []
-thelist.add(int(input("Give some numbers for a list (e.g. 1, 2, 3, 4) :")))
-print(thelist)
+largest=None
+thelist = [1, 2, 3, 9, 40, 4, 5, 6, 9, 0]
+print("Before: ", largest)
+print("-------------------")
+for number in thelist:
+    listtwo=[]
+    listtwo.append(number)
+    print("Loop:    ",number,"  ",max(listtwo))
+print("-------------------")
+print("The largest is:",max(thelist))
+
+listofnumbers=[]
+
+numbers = None
+
+while numbers != 0:
+ numbers=input("Give some numbers: ")
+ for number in numbers:
+    listofnumbers.append(number)
+print(listofnumbers)
+
+sumlist=int(sum(listofnumbers))
+print(sumlist)
+count=int(len(listofnumbers))
+print(count)
+
+print("the average is: ",average= sumlist / count)
+
+
+
+
+
+
+
+
+    
