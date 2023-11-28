f6d32874496a589e65ba13e5a838266ee794fe3a
Hannaert Marie <marie.hannaert@student.howest.be>
2023-11-28 10:40:30 +0100

    Changes to be committed: 	modified:   3. comprehensions-statements/exercise3.3.py 	new file:   3. comprehensions-statements/exercise3.4.py 	new file:   3. comprehensions-statements/exercise3.5.py

---
+++ 3. comprehensions-statements/exercise3.4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+countries={'Belgium':'Brussels', 'England':'London', 'France':'Paris', 'Spain':'Madrid'}
+
+print("List of capitals:\n=================")
+for k, v in countries.items():
+    print("{}".format(v))
+
+astriks=int(input("give a number for astriksjes: "))
---
+++ 3. comprehensions-statements/exercise3.5.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import math
+thelist = []
+thelist.add(int(input("Give some numbers for a list (e.g. 1, 2, 3, 4) :")))
+print(thelist)
