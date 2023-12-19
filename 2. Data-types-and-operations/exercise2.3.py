6dbb3adfe0ad862b97b5fd20e80ea28115f4b928
Marie Hannaert <145533369+MarieHannaert@users.noreply.github.com>
2023-12-19 13:46:43 +0100

    Delete 2. Data-types-and-operations directory

--- 2. Data-types-and-operations/exercise2.3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-charged = ["Arg", "Lys"]
-print(charged)
-secondcharged = ["Asp", "Glu"]
-charged.extend(secondcharged)
-print(charged)
-
--- 2. Data-types-and-operations/exercise2.5.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import math
-a=int(input("what is the lengt of the first side of the triangle? "))
-b=int(input("what is the lengt of the second side of the triangle? "))
-c=int(input("what is the lengt of the thirth side of the triangle? "))
-s=(((a+b+c))/2)
-A=math.sqrt((s*((s-a)*(s-b)*(s-c))))
-print("the area is ", round(A,2))
